0fbc822664a9b5b5dd409cb99a5a90ba73fe6d0e
Guybrush Threepwood <lucianoumc1@gmail.com>
2022-06-20 21:35:03 -0300

    [Fixed] cv download

--- src/components/Cover/index.jsx
+++ src/components/Cover/index.jsx
@@ -1,29 +1,28 @@
-import Buttton from '../Button'
-import cv from '../../assets/cv.pdf'
-import './Cover.scss'
+import Buttton from "../Button";
+import cv from "../../assets/Luciano Madronal-CV.pdf";
+import "./Cover.scss";
 
 export default function Cover() {
-  
   const contactHandleClickContact = () => {
-    document.location.href = '#contact-section';
-  }
-  const contactHandleClickCv = () => {
-    document.location.href = cv ;
-  }
-
+    document.location.href = "#contact-section";
+  };
   return (
-    <div className='cover__section' id='home'>
-      <div className='cover__content'>
-        <div className='cover__tittle-principal'>
-          <span	>👋</span>
+    <div className="cover__section" id="home">
+      <div className="cover__content">
+        <div className="cover__tittle-principal">
+          <span>👋</span>
           <h1> Hola, soy Luciano</h1>
         </div>
-        <h2>Soy un desarrollador Front-End y aficionado de la tecnolog&iacute;a 🖥️</h2>
-        <div className='cover-content__buttons'>
-          <Buttton name='Contacto' handleClick={contactHandleClickContact} />
-          <Buttton name='CV' handleClick={contactHandleClickCv} />
+        <h2>
+          Soy un desarrollador Front-End y aficionado de la tecnolog&iacute;a 🖥️
+        </h2>
+        <div className="cover-content__buttons">
+          <Buttton name="Contacto" handleClick={contactHandleClickContact} />
+          <a href={cv} download>
+            <Buttton name="CV" />
+          </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
